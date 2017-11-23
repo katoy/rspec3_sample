a8f290fb2ab01eccd3a2a4e749de1574bf4e6d8f
kato_youichi <youichikato@gmail.com>
2017-11-23 20:45:23 +0900

    query 機能の spec を追加し、PASS させる

--- app/ledger.rb
+++ app/ledger.rb
@@ -20,5 +20,9 @@ module ExpenseTracker
       id = DB[:expenses].max(:id)
       RecordResult.new(true, id, nil)
     end
+
+    def expenses_on(date)
+      DB[:expenses].where(date: date).all
+    end
   end
 end
