fc1683ddf70415b4de448c80bcc5d8faa0e11f35
kato_youichi <youichikato@gmail.com>
2017-11-24 20:35:36 +0900

    GET /expeses/;date の SPEC がぬけていたのを修正

--- spec/unit/app/api_spec.rb
+++ spec/unit/app/api_spec.rb
@@ -21,12 +21,6 @@ module ExpenseTracker
 
     let(:ledger) { instance_double('ExpenseTracker::Ledger') }
 
-    before do
-      allow(ledger).to receive(:record)
-        .with(expense)
-        .and_return(RecordResult.new(true, 417, nil))
-    end
-
     describe 'POST /expenses' do
       context 'when the expense is successfully recorded' do
         let(:expense) { { 'some' => 'data' } }
@@ -69,6 +63,48 @@ module ExpenseTracker
         it 'responds with a 422 (Unprocessable entity)' do
           post '/expenses', JSON.generate(expense)
           expect(last_response.status).to eq(422)
+        end
+      end
+    end
+
+    describe 'GET /expenses/:date' do
+      context 'when expenses exist on the given date' do
+        before do
+          allow(ledger).to receive(:expenses_on)
+            .with('2017-06-12')
+            .and_return(['expense_1', 'expense_2'])
+        end
+
+        it 'returns the expense records as JSON' do
+          get '/expenses/2017-06-12'
+
+          parsed = JSON.parse(last_response.body)
+          expect(parsed).to eq(['expense_1', 'expense_2'])
+        end
+
+        it 'responds with a 200 (OK)' do
+          get '/expenses/2017-06-12'
+          expect(last_response.status).to eq(200)
+        end
+      end
+
+      context 'when there are no expenses on the given date' do
+        before do
+          allow(ledger).to receive(:expenses_on)
+            .with('2017-06-12')
+            .and_return([])
+        end
+
+        it 'returns an empty array as JSON' do
+          get '/expenses/2017-06-12'
+
+          parsed = JSON.parse(last_response.body)
+          expect(parsed).to eq([])
+        end
+
+        it 'responds with a 200 (OK)' do
+          get '/expenses/2017-06-12'
+          expect(last_response.status).to eq(200)
         end
       end
     end
